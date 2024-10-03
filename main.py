a5d37b40a19bdd681d133989870655452f20cc55
ILoveRice07 <159123610+ILoveRice07@users.noreply.github.com>
2024-10-03 11:46:42 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-#vytvoření pole
-array = [21, 7, 1, 38, 25, 94, 4, 52, 75]
+#pole
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print (array)
 
-#vypsání první, prostřední a poslední hodnoty
-print()
+#výměna čísla v poli a výměna zpátky
+array[5] = 7
+print (array)
+
+#délka pole a součet
+delka = len(array)
+print (delka)
+
+soucet = sum(array)
+print (soucet)
+
+#druhé pole
+arrayB = [9, 8, 8, 5, 5, 2, 77, 91, 78, 48]
+
+#součet obou polí
+print (sum(array+arrayB))
+
+#délka obou polí
+print (len(array+arrayB))
