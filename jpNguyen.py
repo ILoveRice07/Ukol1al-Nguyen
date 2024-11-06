60e2e3262d06b489543354dfbff9aa2d995fff9f
ILoveRice07 <159123610+ILoveRice07@users.noreply.github.com>
2024-11-06 11:19:12 +0100

    final_Nguyen

---
+++ jpNguyen.py
@@ -0,0 +1,22 @@
+#Úkol 1-Výtvoření pole
+array = ["měsíc", "den", "čtverec", "kláda", "sešit",]
+print(array)
+
+#Úkol 2-Přidání prvku vítr
+array.append("vítr")
+print(array)
+
+#Úkol 3-Odstranění druhého prvku z pole
+array.remove(array[1])
+print(array)
+
+#Úkol 4-Záměna páté hodnoty za "slunce"
+array[4] = "slunce"
+print(array)
+
+#Úkol 5-Přídání dvou stringových hodnot
+arrayB = ["kámen", "kruh"]
+array.extend(arrayB)
+print(array)
+
+#Úkol 6-Vypsání změn
